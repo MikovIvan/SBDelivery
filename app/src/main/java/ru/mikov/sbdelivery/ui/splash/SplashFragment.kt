bce9a9afb5663aaae6d0bd70a5a386f9e6e826fd
Ivan <mikov_ivan@list.ru>
2020-07-21 20:00:57 +0300

    add base classes

--- app/src/main/java/ru/mikov/sbdelivery/ui/splash/SplashFragment.kt
+++ app/src/main/java/ru/mikov/sbdelivery/ui/splash/SplashFragment.kt
@@ -1,32 +1,34 @@
 package ru.mikov.sbdelivery.ui.splash
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import kotlinx.android.synthetic.main.activity_root.*
+import kotlinx.android.synthetic.main.fragment_splash.*
 import ru.mikov.sbdelivery.R
+import ru.mikov.sbdelivery.ui.base.BaseFragment
+import ru.mikov.sbdelivery.ui.base.Binding
+import ru.mikov.sbdelivery.viewmodels.base.IViewModelState
+import ru.mikov.sbdelivery.viewmodels.base.NavigationCommand
+import ru.mikov.sbdelivery.viewmodels.splash.SplashViewModel
 
-class SplashFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SplashFragment()
+class SplashFragment : BaseFragment<SplashViewModel>() {
+
+    override val viewModel: SplashViewModel by activityViewModels()
+    override val layout: Int = R.layout.fragment_splash
+    override val binding: SplashBinding by lazy { SplashBinding() }
+
+    override fun setupViews() {
+        requireActivity()!!.appbar.visibility = View.GONE
+
+        iv_logo.setOnClickListener { viewModel.navigate(NavigationCommand.To(R.id.nav_main)) }
+
     }
 
-    private lateinit var viewModel: SplashViewModel
+    inner class SplashBinding : Binding() {
+        override fun bind(data: IViewModelState) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        }
+
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
