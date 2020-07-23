23ce34e41b3b5d04b36dca831056762cee5edfc5
Ivan <mikov_ivan@list.ru>
2020-07-23 21:36:43 +0300

    add base classes

--- app/src/main/java/ru/mikov/sbdelivery/viewmodels/menu/MenuViewModel.kt
+++ app/src/main/java/ru/mikov/sbdelivery/viewmodels/menu/MenuViewModel.kt
@@ -1,7 +1,13 @@
 package ru.mikov.sbdelivery.viewmodels.menu
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.SavedStateHandle
+import ru.mikov.sbdelivery.viewmodels.base.BaseViewModel
+import ru.mikov.sbdelivery.viewmodels.base.IViewModelState
 
-class MenuViewModel : ViewModel() {
+class MenuViewModel(handle: SavedStateHandle) : BaseViewModel<MenuState>(handle, MenuState()) {
     // TODO: Implement the ViewModel
 }
+
+data class MenuState(
+    val isSearch: Boolean = false
+) : IViewModelState
