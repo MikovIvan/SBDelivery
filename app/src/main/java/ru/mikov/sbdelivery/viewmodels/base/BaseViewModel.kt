37d0621d7f89c0671a20ce2ac5c6afb266a7b632
Ivan <mikov_ivan@list.ru>
2020-09-07 22:29:08 +0300

    add launchSafety fun

--- app/src/main/java/ru/mikov/sbdelivery/viewmodels/base/BaseViewModel.kt
+++ app/src/main/java/ru/mikov/sbdelivery/viewmodels/base/BaseViewModel.kt
@@ -6,6 +6,13 @@ import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.*
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigator
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+import ru.mikov.sbdelivery.data.remote.err.ApiError
+import ru.mikov.sbdelivery.data.remote.err.NoNetworkError
+import java.net.SocketTimeoutException
 
 abstract class BaseViewModel<T : IViewModelState>(
     private val handleState: SavedStateHandle,
@@ -16,6 +23,8 @@ abstract class BaseViewModel<T : IViewModelState>(
 
     @VisibleForTesting(otherwise = VisibleForTesting.PROTECTED)
     val navigation = MutableLiveData<Event<NavigationCommand>>()
+
+    private val loading = MutableLiveData<Loading>(Loading.HIDE_LOADING)
 
     /***
      * Инициализация начального состояния аргументом конструктоа, и объявления состояния как
@@ -53,6 +62,20 @@ abstract class BaseViewModel<T : IViewModelState>(
     @UiThread
     protected fun notify(content: Notify) {
         notifications.value = Event(content)
+    }
+
+    /***
+     * отображение индикатора загрузки ( по умолчанию не блокирующий Loading
+     */
+    protected fun showLoading(loadingType: Loading = Loading.SHOW_LOADING) {
+        loading.value = loadingType
+    }
+
+    /***
+     * скрытие отображения загрузки
+     */
+    protected fun hideLoading() {
+        loading.value = Loading.HIDE_LOADING
     }
 
     open fun navigate(command: NavigationCommand) {
@@ -107,6 +130,43 @@ abstract class BaseViewModel<T : IViewModelState>(
         state.value = currentState.restore(handleState) as T
     }
 
+    protected fun launchSafety(
+        errHandler: ((Throwable) -> Unit)? = null,
+        compHandler: ((Throwable?) -> Unit)? = null,
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        //используется обработчик ошибок переданный в качестве аргумента или обработчик ошибок по умолчанию
+        val errHand = CoroutineExceptionHandler { _, err ->
+            errHandler?.invoke(err) ?: when (err) {
+                is NoNetworkError -> notify(Notify.TextMessage("Network not available, check internet connection"))
+
+                is SocketTimeoutException -> notify(
+                    Notify.ActionMessage(
+                        "Network timeout exeption - please try again",
+                        "Retry"
+                    ) { launchSafety(errHandler, compHandler, block) })
+
+                is ApiError.InternalServerError -> notify(
+                    Notify.ErrorMessage(
+                        err.message,
+                        "Retry",
+                        { launchSafety(errHandler, compHandler, block) })
+                )
+
+                is ApiError -> notify(Notify.ErrorMessage(err.message))
+                else -> notify(Notify.ErrorMessage(err.message ?: "Something wrong"))
+            }
+        }
+
+        (viewModelScope + errHand).launch {
+            showLoading()
+            block()
+        }.invokeOnCompletion {
+            hideLoading()
+            compHandler?.invoke(it)
+        }
+    }
+
 }
 
 class Event<out E>(private val content: E) {
@@ -154,8 +214,8 @@ sealed class Notify {
 
     data class ErrorMessage(
         override val message: String,
-        val errLabel: String?,
-        val errHandler: (() -> Unit)?
+        val errLabel: String? = null,
+        val errHandler: (() -> Unit)? = null
     ) : Notify()
 }
 
@@ -175,4 +235,8 @@ sealed class NavigationCommand() {
         val privateDestination: Int? = null
     ) : NavigationCommand()
 
+}
+
+enum class Loading {
+    SHOW_LOADING, SHOW_BLOCKING_LOADING, HIDE_LOADING
 }
