039c6a2001ee17f01d3334cb4bea71ccccd4c7d1
Ivan <mikov_ivan@list.ru>
2020-09-07 22:27:53 +0300

    add registrationFragment+viewmodel

--- app/src/main/java/ru/mikov/sbdelivery/ui/auth/RegistrationFragment.kt
+++ app/src/main/java/ru/mikov/sbdelivery/ui/auth/RegistrationFragment.kt
@@ -1,23 +1,151 @@
 package ru.mikov.sbdelivery.ui.auth
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import kotlinx.android.synthetic.main.fragment_registration.*
 import ru.mikov.sbdelivery.R
-import ru.mikov.sbdelivery.extensions.*
+import ru.mikov.sbdelivery.extensions.invisible
+import ru.mikov.sbdelivery.extensions.visible
 import ru.mikov.sbdelivery.ui.base.BaseFragment
+import ru.mikov.sbdelivery.ui.base.Binding
+import ru.mikov.sbdelivery.ui.delegates.RenderProp
+import ru.mikov.sbdelivery.viewmodels.auth.RegistrationState
 import ru.mikov.sbdelivery.viewmodels.auth.RegistrationViewModel
+import ru.mikov.sbdelivery.viewmodels.base.IViewModelState
 
 class RegistrationFragment : BaseFragment<RegistrationViewModel>() {
     override val viewModel: RegistrationViewModel by viewModels()
     override val layout: Int = R.layout.fragment_registration
+    override val binding: RegistrationBinding by lazy { RegistrationBinding() }
 
     override fun setupViews() {
-        et_name.afterTextChanged {
-            if (it.hasDigits() && it.isNotBlank()) tv_error_name.visible() else tv_error_name.invisible()
+        et_name.addTextChangedListener(textWatcher)
+        et_surname.addTextChangedListener(textWatcher)
+        et_login.addTextChangedListener(textWatcher)
+        et_password.addTextChangedListener(textWatcher)
+
+        btn_registration.setOnClickListener {
+            if (!binding.isRegBtnEnable) {
+                viewModel.checkFields()
+            } else if (viewModel.checkValidation(
+                    binding.name,
+                    binding.surname,
+                    binding.login,
+                    binding.password
+                )
+            ) {
+                //            viewModel.handleRegistration(
+//                et_name.text.toString(),
+//                et_surname.text.toString(),
+//                et_login.text.toString(),
+//                et_password.text.toString()
+//            )
+            }
+
+
+
+            Toast.makeText(context, "Click", Toast.LENGTH_SHORT).show()
+
         }
-        et_surname.afterTextChanged { if (it.hasDigits() && it.isNotBlank()) tv_error_surname.visible() else tv_error_surname.invisible() }
-        et_login.afterTextChanged { if (!it.isEmailValid()) tv_error_login.visible() else tv_error_login.invisible() }
-        et_password.afterTextChanged { if (it.isBlank()) tv_error_password.visible() else tv_error_password.invisible() }
     }
 
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) = viewModel.handleBtn()
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            viewModel.handleName(et_name.text.toString())
+            viewModel.handleSurname(et_surname.text.toString())
+            viewModel.handleLogin(et_login.text.toString())
+            viewModel.handlePassword(et_password.text.toString())
+        }
+    }
+
+    inner class RegistrationBinding : Binding() {
+        var name: String = ""
+        var surname: String = ""
+        var login: String = ""
+        var password: String = ""
+
+        var isRegBtnEnable: Boolean by RenderProp(false) {
+            btn_registration.isActivated = it
+        }
+
+        var nameError: Boolean by RenderProp(false) {
+            with(tv_error_name) {
+                text = "Empty field"
+                if (it) visible() else invisible()
+            }
+        }
+
+        var surnameError: Boolean by RenderProp(false) {
+            with(tv_error_surname) {
+                text = "Empty field"
+                if (it) visible() else invisible()
+            }
+        }
+
+        var loginError: Boolean by RenderProp(false) {
+            with(tv_error_login) {
+                text = "Empty field"
+                if (it) visible() else invisible()
+            }
+        }
+
+        var passwordError: Boolean by RenderProp(false) {
+            with(tv_error_password) {
+                text = "Empty field"
+                if (it) visible() else invisible()
+            }
+        }
+
+        var isNameValid: Boolean by RenderProp(true) {
+            with(tv_error_name) {
+                text = "Only letters"
+                if (!it) visible() else invisible()
+            }
+        }
+
+        var isSurnameValid: Boolean by RenderProp(true) {
+            with(tv_error_surname) {
+                text = "Only letters"
+                if (!it) visible() else invisible()
+            }
+        }
+
+        var isLoginValid: Boolean by RenderProp(true) {
+            with(tv_error_login) {
+                text = "Invalid e-mail"
+                if (!it) visible() else invisible()
+            }
+        }
+
+        var isPasswordValid: Boolean by RenderProp(true) {
+            with(tv_error_password) {
+                text = "Empty field"
+                if (!it) visible() else invisible()
+            }
+        }
+
+        override fun bind(data: IViewModelState) {
+            data as RegistrationState
+            name = data.name
+            surname = data.surname
+            login = data.login
+            password = data.password
+            nameError = data.nameError
+            surnameError = data.surnameError
+            loginError = data.loginError
+            passwordError = data.passwordError
+            isNameValid = data.isNameValid
+            isSurnameValid = data.isSurnameValid
+            isLoginValid = data.isLoginValid
+            isPasswordValid = data.isPasswordValid
+            isRegBtnEnable = data.isRegBtnEnable
+        }
+
+    }
 }
--- app/src/main/java/ru/mikov/sbdelivery/viewmodels/auth/RegistrationViewModel.kt
+++ app/src/main/java/ru/mikov/sbdelivery/viewmodels/auth/RegistrationViewModel.kt
@@ -2,12 +2,68 @@ package ru.mikov.sbdelivery.viewmodels.auth
 
 import androidx.lifecycle.SavedStateHandle
 import ru.mikov.sbdelivery.data.repositories.RootRepository
+import ru.mikov.sbdelivery.extensions.hasDigits
+import ru.mikov.sbdelivery.extensions.isEmailValid
 import ru.mikov.sbdelivery.viewmodels.base.BaseViewModel
 import ru.mikov.sbdelivery.viewmodels.base.IViewModelState
 
 class RegistrationViewModel(handle: SavedStateHandle) :
     BaseViewModel<RegistrationState>(handle, RegistrationState()) {
     private val repository = RootRepository
+
+    fun handleRegistration(name: String, surname: String, login: String, password: String) {
+        launchSafety {
+            repository.register(name, surname, login, password)
+        }
+    }
+
+    fun handleName(name: String) {
+        updateState { it.copy(name = name, nameError = false) }
+    }
+
+    fun handleSurname(surname: String) {
+        updateState { it.copy(surname = surname, surnameError = false) }
+    }
+
+    fun handleLogin(login: String) {
+        updateState { it.copy(login = login, loginError = false) }
+    }
+
+    fun handlePassword(password: String) {
+        updateState { it.copy(password = password, passwordError = false) }
+    }
+
+    fun handleBtn() {
+        updateState {
+            it.copy(
+                isRegBtnEnable = currentState.name.isNotBlank() && currentState.surname.isNotBlank()
+                        && currentState.login.isNotBlank() && currentState.password.isNotBlank()
+            )
+        }
+    }
+
+    fun checkValidation(name: String, surname: String, login: String, password: String): Boolean {
+        updateState {
+            it.copy(
+                isNameValid = !name.hasDigits(),
+                isSurnameValid = !surname.hasDigits(),
+                isLoginValid = login.isEmailValid(),
+                isPasswordValid = !password.isEmpty()
+            )
+        }
+        return (currentState.isNameValid && currentState.isSurnameValid && currentState.isLoginValid && currentState.isPasswordValid)
+    }
+
+    fun checkFields() {
+        updateState {
+            it.copy(
+                nameError = currentState.name.isEmpty(),
+                surnameError = currentState.surname.isEmpty(),
+                loginError = currentState.login.isEmpty(),
+                passwordError = currentState.password.isEmpty()
+            )
+        }
+    }
 
 
 }
@@ -16,5 +72,31 @@ data class RegistrationState(
     val name: String = "",
     val surname: String = "",
     val login: String = "",
-    val password: String = ""
-) : IViewModelState
+    val password: String = "",
+    val nameError: Boolean = false,
+    val surnameError: Boolean = false,
+    val loginError: Boolean = false,
+    val passwordError: Boolean = false,
+    val isNameValid: Boolean = true,
+    val isSurnameValid: Boolean = true,
+    val isLoginValid: Boolean = true,
+    val isPasswordValid: Boolean = true,
+    val isRegBtnEnable: Boolean = false
+
+) : IViewModelState {
+    override fun save(outState: SavedStateHandle) {
+        outState.set("name", name)
+        outState.set("surname", surname)
+        outState.set("login", login)
+        outState.set("password", password)
+    }
+
+    override fun restore(savedState: SavedStateHandle): IViewModelState {
+        return copy(
+            name = savedState["name"] ?: "",
+            surname = savedState["surname"] ?: "",
+            login = savedState["login"] ?: "",
+            password = savedState["password"] ?: ""
+        )
+    }
+}
