432ab212e630f1d12417da9b2e96316d511f4d16
Ivan <mikov_ivan@list.ru>
2020-09-09 22:45:54 +0300

    add connectTimeout

--- app/src/main/java/ru/mikov/sbdelivery/data/remote/NetworkManager.kt
+++ app/src/main/java/ru/mikov/sbdelivery/data/remote/NetworkManager.kt
@@ -1,27 +1,23 @@
 package ru.mikov.sbdelivery.data.remote
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import ru.mikov.sbdelivery.AppConfig
+import ru.mikov.sbdelivery.data.JsonConverter.moshi
 import ru.mikov.sbdelivery.data.remote.interceptors.ErrorStatusInterceptor
 import ru.mikov.sbdelivery.data.remote.interceptors.NetworkStatusInterceptor
 import java.util.concurrent.TimeUnit
 
 object NetworkManager {
     val api: RestService by lazy {
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-
         val logging = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
 
         val client = OkHttpClient().newBuilder()
+            .connectTimeout(3, TimeUnit.SECONDS)
             .readTimeout(2, TimeUnit.SECONDS)
             .writeTimeout(5, TimeUnit.SECONDS)
             .addInterceptor(NetworkStatusInterceptor())
