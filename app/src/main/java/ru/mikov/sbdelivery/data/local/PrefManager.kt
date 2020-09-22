bea77181a7d185e362a0f0599aea1f7b4e761f2d
Ivan <mikov_ivan@list.ru>
2020-09-22 21:50:36 +0300

    add navheader and logout

--- app/src/main/java/ru/mikov/sbdelivery/data/local/PrefManager.kt
+++ app/src/main/java/ru/mikov/sbdelivery/data/local/PrefManager.kt
@@ -5,8 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import androidx.preference.PreferenceManager
 import ru.mikov.sbdelivery.App
+import ru.mikov.sbdelivery.data.JsonConverter.moshi
 import ru.mikov.sbdelivery.data.delegates.PrefDelegate
 import ru.mikov.sbdelivery.data.delegates.PrefLiveDelegate
+import ru.mikov.sbdelivery.data.delegates.PrefLiveObjDelegate
+import ru.mikov.sbdelivery.data.delegates.PrefObjDelegate
 import ru.mikov.sbdelivery.data.models.User
 
 object PrefManager {
@@ -16,12 +19,14 @@ object PrefManager {
 
     var accessToken by PrefDelegate("")
     var refreshToken by PrefDelegate("")
-    var profile: User? = null
+    var profile: User? by PrefObjDelegate(moshi.adapter(User::class.java))
 
     val isAuthLive: LiveData<Boolean> by lazy {
         val token by PrefLiveDelegate("accessToken", "", preferences)
         token.map { it.isNotEmpty() }
     }
+
+    val profileLive: LiveData<User?> by PrefLiveObjDelegate("profile", moshi.adapter(User::class.java), preferences)
 
     fun clearAll() {
         preferences.edit()
