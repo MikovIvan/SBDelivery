0bbc1765bafd9a5e4aa84bff57b96ef5ca78ff37
Ivan <mikov_ivan@list.ru>
2020-09-30 22:45:43 +0300

    change type of selectedSort and add key CHOOSE_SELECTED_SORT

--- app/src/main/java/ru/mikov/sbdelivery/ui/dialogs/SortCategoryDialog.kt
+++ app/src/main/java/ru/mikov/sbdelivery/ui/dialogs/SortCategoryDialog.kt
@@ -8,50 +8,51 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.navArgs
+import ru.mikov.sbdelivery.R
 
 class SortCategoryDialog : DialogFragment() {
     companion object {
+        const val CHOOSE_SELECTED_SORT = "CHOOSE_SELECTED_SORT"
         const val SELECTED_SORT = "SELECTED_SORT"
     }
 
     private val args: SortCategoryDialogArgs by navArgs()
-    private val selectedSort: Int = -1
+    private var selectedSort: String = ""
     private val sortItems = arrayOf("A to Z", "Z to A", "More popular", "Less popular", "Higher rating", "Lower rating")
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-        selectedSort == -1
-        selectedSort == savedInstanceState?.getInt("selectedSort") ?: args.selectedSort
-
+        selectedSort = ""
+        selectedSort = savedInstanceState?.getString("selectedSort") ?: args.selectedSort
         val tv = TextView(context)
         with(tv) {
-            text = "Sort"
+            text = context.getString(R.string.sort)
             textSize = 20F
             setTextColor(resources.getColor(android.R.color.black))
             setPadding(20, 30, 20, 30)
         }
         val adb = AlertDialog.Builder(requireContext())
             .setCustomTitle(tv)
-            .setSingleChoiceItems(sortItems, -1) { dialog, item ->
-                selectedSort == item
+            .setSingleChoiceItems(sortItems, sortItems.indexOf(selectedSort)) { dialog, item ->
+                selectedSort = sortItems[item]
             }
             .setPositiveButton("Apply") { _, _ ->
                 setFragmentResult(
-                    SELECTED_SORT,
+                    CHOOSE_SELECTED_SORT,
                     bundleOf(SELECTED_SORT to selectedSort)
                 )
             }
             .setNegativeButton("Reset") { _, _ ->
                 setFragmentResult(
-                    SELECTED_SORT,
-                    bundleOf(SELECTED_SORT to -1)
+                    CHOOSE_SELECTED_SORT,
+                    bundleOf(SELECTED_SORT to "")
                 )
             }
         return adb.create()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt("selectedSort", selectedSort)
+        outState.putString("selectedSort", selectedSort)
         super.onSaveInstanceState(outState)
     }
 }
